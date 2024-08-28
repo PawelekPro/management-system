c42864a50e6c779ba99cd07a8a2002b51fedf92f
pgilewicz <pawel.gilewicz1@gmail.com>
2024-08-28 21:00:00 +0200

    devel (index.php): changed stylesheet of home page

--- index.php
+++ index.php
@@ -2,71 +2,74 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="assets/css/style.css" rel="stylesheet">
 
-  <title>Vacation Management System</title>
-  <style>
+    <title>Vacation Management System</title>
+    <style>
     body,
     html {
-      height: 100%;
-      margin: 0;
+        height: 100%;
+        margin: 0;
     }
-  </style>
+    </style>
 </head>
 
 <body>
-  <div class="bg">
-    <div class="login-form-bg h-100">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xl-6">
-            <div class="form-input-content">
-              <div class="card login-form mt-5">
-                <div class="card-body shadow">
+    <div class="bg">
+        <div class="login-form-bg h-100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="form-input-content">
+                            <div class="card login-form mt-5">
+                                <div class="card-body shadow">
 
-                  <h2 class="text-center pb-4">Vacation Management System</h2>
-                  <h6 class="text-center pb-4">Please Log-In according to your role to continue.</h6>
+                                    <h2 class="text-center pb-4">Vacation Management System</h2>
+                                    <h6 class="text-center pb-4">Please Log-In according to your role to continue.</h6>
 
-                  <div class="container mt-4">
-                    <div class="btn-toolbar justify-content-between">
-                      <div class="btn-group">
-                        <a href="src/User/dashboard.php" class="btn btn-primary btn-lg">Log-in As User</a>
-                      </div>
+                                    <div class="container mt-4">
+                                        <div class="btn-toolbar justify-content-between">
+                                            <div class="btn-group">
+                                                <a href="src/User/dashboard.php" class="btn btn-primary btn-lg">Log-in
+                                                    As User</a>
+                                            </div>
 
-                      <div class="btn-group">
-                        <a href="src/Admin/dashboard.php" class="btn btn-primary btn-lg">Log-In As Admin</a>
-                      </div>
+                                            <div class="btn-group">
+                                                <a href="src/Admin/dashboard.php" class="btn btn-primary btn-lg">Log-In
+                                                    As Admin</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 
-  <!-- Footer Section -->
-  <footer class="text-center mt-5">
-    <p class="mb-0">
-      Vacation Management System &copy; 2024 | Developed by Paweł Gilewicz |
-      <a href="https://github.com/PawelekPro" target="_blank">PawelekPro</a>
-    </p>
-  </footer>
+    <!-- Footer Section -->
+    <footer class="text-center mt-5">
+        <p class="mb-0">
+            Vacation Management System &copy; 2024 | Developed by Paweł Gilewicz |
+            <a href="https://github.com/PawelekPro" target="_blank">PawelekPro</a>
+        </p>
+    </footer>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
     </script>
 
 </body>
