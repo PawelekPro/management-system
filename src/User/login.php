9aca1ed956505d4f9f3b5f7bfb972ec7950050d0
pgilewicz <pawel.gilewicz1@gmail.com>
2024-08-28 21:02:15 +0200

    fix(assets): fixed paths to resources

--- src/User/login.php
+++ src/User/login.php
@@ -9,15 +9,15 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link href="../resorce/css/style.css" rel="stylesheet">
+    <link href="../../assets/css/style.css" rel="stylesheet">
 
     <title>Vacation Management System</title>
     <style>
-    body,
-    html {
-        height: 100%;
-        margin: 0;
-    }
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+        }
     </style>
 </head>
 
@@ -68,10 +68,10 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
