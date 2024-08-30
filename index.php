1b9c5a8849814e5898d594be87b9a0328f8ab0cf
pgilewicz <pawel.gilewicz1@gmail.com>
2024-08-30 20:08:52 +0200

    Removed scripts that seems not to work

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
 
     <title>Vacation Management System</title>
     <style>
-    body,
-    html {
-        height: 100%;
-        margin: 0;
-    }
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+        }
     </style>
 </head>
 
@@ -71,13 +71,6 @@
             <a href="https://github.com/PawelekPro" target="_blank">PawelekPro</a>
         </p>
     </footer>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
-    </script>
 
 </body>
 
